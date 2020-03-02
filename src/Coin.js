7e88e5c3b97477c5a8ee8e58c77c3bc9b6029ef7
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-03-02 11:50:20 -0500

    adding loonie

--- src/Coin.js
+++ src/Coin.js
@@ -7,7 +7,7 @@ export default class Coin extends Component {
         {this.props.coinType === "head" ? (
           <img
             style={{ width: "10%", height: "auto" }}
-            src="https://tinyurl.com/react-coin-heads-jpg"
+            src="https://upload.wikimedia.org/wikipedia/en/e/e0/Canadian_Dollar_-_obverse.png"
             alt={this.props.coinType}
           />
         ) : (
@@ -16,7 +16,7 @@ export default class Coin extends Component {
         {this.props.coinType === "tail" ? (
           <img
             style={{ width: "10%", height: "auto" }}
-            src="http://www.pcgscoinfacts.com/UserImages/71009269r.jpg?__cf_chl_jschl_tk__=55f472b01f8ce124957c2327d16ec40f5467d61f-1583164275-0-ARPox93W1oWQ24o_PsJb3bSZ5HCbe1pHFmZYe4JtZQ11XFyp1EQZ91FNYXZmQakpoPJ1zMX9grk1ynbu06CWoVRukq6HmNpr9flmTjwlLGChW5v-Nnw_Fe9v2VGvAnrQ5tYPVtUKVhp5D3ZcIyNVqy5lSp2rKC2BEdD0vyprFTWe7-bwDHo-wrQB1rZ21Fv5EdMgFWu_fKvQLIoZDF-QAMf-nEiGXK6nAfspnm6MsPsnVt-PhBqb3bNV9zCez9yTeGyaIjR9Eed0kWNK7qe-shINASyIQrqxgs0v-k4Suj8RLlhqj0CJsnbtvwXGXwJzQw"
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/e/ef/Canadian_Dollar_-_reverse.png/220px-Canadian_Dollar_-_reverse.png"
             alt={this.props.coinType}
           />
         ) : (
